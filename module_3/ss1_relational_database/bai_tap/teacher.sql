09bc0d8c813628accf81fc11efe65f31173a1260
unknown <levanphatdat9@gmail.com>
2023-01-11 10:31:28 +0700

    baitap

--- module_3/ss1_relational_database/bai_tap/teacher.sql
+++
@@ -1,9 +0,0 @@
-use student_management;
-create table `teacher` (
-    id int not null,
-    `name` varchar(45),
-    age int not null,
-    country varchar(45),
-    primary key (id));
-insert into teacher (id,name, age, country) values (1, "Hai",34, "Hue");
-select * from teacher;
