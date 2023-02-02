ccb2d4f1a5816d67e095e107fb64d473984d7cfa
unknown <levanphatdat9@gmail.com>
2023-02-02 09:50:39 +0700

    baitap

--- module_3/case_study/query_furama.sql
+++ module_3/case_study/query_furama.sql
@@ -169,3 +169,17 @@ where year(hd.ngay_lam_hop_dong) between 2020 and 2021
 group by nv.ma_nhan_vien
 having count(nv.ma_nhan_vien) <= 3
 order by nv.ma_nhan_vien;
+
+-- 16.	Xóa những Nhân viên chưa từng lập được hợp đồng nào từ năm 2019 đến năm 2021.
+
+
+
+-- 17.	Cập nhật thông tin những khách hàng có ten_loai_khach từ Platinum lên Diamond, 
+-- chỉ cập nhật những khách hàng đã từng đặt phòng với Tổng Tiền thanh toán trong năm 2021 là lớn hơn 10.000.000 VNĐ.
+
+-- 18.	Xóa những khách hàng có hợp đồng trước năm 2021 (chú ý ràng buộc giữa các bảng).
+
+-- 19.	Cập nhật giá cho các dịch vụ đi kèm được sử dụng trên 10 lần trong năm 2020 lên gấp đôi.
+
+-- 20.	Hiển thị thông tin của tất cả các nhân viên và khách hàng có trong hệ thống, thông tin hiển thị 
+-- bao gồm id (ma_nhan_vien, ma_khach_hang), ho_ten, email, so_dien_thoai, ngay_sinh, dia_chi.
--- module_3/ss5_sql_view_index_&_stored_procedure/bai_tap/products.sql
+++ module_3/ss5_sql_view_index_&_stored_procedure/bai_tap/products.sql
@@ -3,44 +3,44 @@ create database product;
 use product;
 
 create table products (
-id int primary key,
-code varchar(45),
-name varchar(45),
-price double,
-amount int,
-description varchar(45),
-status varchar(45)
+p_id int primary key,
+p_code varchar(45),
+p_name varchar(45),
+p_price double,
+p_amount int,
+p_description varchar(45),
+p_status varchar(45)
 );
 
-insert into products (id, code, name, price, amount, description, status)
+insert into products (p_id, p_code, p_name, p_price, p_amount, p_description, p_status)
 values (1, 'PR1', 'quat', 250000, 3, 'ranh', 'tot'),
 (2, 'PR2', 'ban', 550000, 1, 'ranh', 'binh thuong'),
 (3, 'PR3', 'ghe', 300000, 4, 'ranh', 'binh thuong');
 
 select * from products;
 
-create unique index code_index
-on products(code);
+create unique index p_code_index
+on products(p_code);
 
 create index composite_index
-on products(name, price);
+on products(p_name, p_price);
 
 explain select * from products;
 
 explain select * from products 
-where code = 'PR2';
+where p_code = 'PR2';
 
 explain select * from products 
-where name = 'ban';
+where p_name = 'ban';
 
-create view view_products(code, name, price, status) as
-select code, name, price, status from products;
+create view view_products as
+select p_code, p_name, p_price, p_status from products;
 
 select * from view_products;
 
 update view_products 
-set name = 'chen'
-where code = 'PR1';
+set p_name = 'chen'
+where p_code = 'PR1';
 
 drop view view_products;
 
@@ -54,52 +54,52 @@ delimiter ;
 call all_information_product();
 
 delimiter //
-create procedure add_information(id int,
-code varchar(45),
-name varchar(45),
-price double,
-amount int,
-description varchar(45),
-status varchar(45))
+create procedure add_information(in p_id int,
+in p_code varchar(45),
+in p_name varchar(45),
+in p_price double,
+in p_amount int,
+in p_description varchar(45),
+in p_status varchar(45))
 begin
-insert into products (id, code, name, price, amount, description, status)
-values (id, code, name, price, amount, description, status);
+insert into products (p_id, p_code, p_name, p_price, p_amount, p_description, p_status)
+values (p_id, p_code, p_name, p_price, p_amount, p_description, p_status);
 end //
 delimiter ;
 
 call add_information(4, 'PR4', 'dua', 100000, 20, 'dang thue', 'binh thuong');
 
 delimiter // 
-create procedure edit_by_id( id int, 
-code varchar(45),
-name varchar(45),
-price double,
-amount int,
-description varchar(45),
-status varchar(45))
+create procedure edit_by_p_id(in p_id int,
+in p_code varchar(45),
+in p_name varchar(45),
+in p_price double,
+in p_amount int,
+in p_description varchar(45),
+in p_status varchar(45))
 begin
 update products p
 set 
-p.code = code,
-p.name = name,
-p.price = price,
-p.amount= amount,
-p.description = description,
-p.status = status
-where p.id = id;
+p.p_code = p_code,
+p.p_name = p_name,
+p.p_price = p_price,
+p.p_amount= p_amount,
+p.p_description = p_description,
+p.p_status = p_status
+where p.p_id = p_id;
 end //
 delimiter ;
 
-call edit_by_id (4, 'PR4', 'muong', 150000, 6, 'ranh', 'tot');
+call edit_by_p_id (4, 'PR4', 'muong', 150000, 6, 'ranh', 'tot');
 
 delimiter //
-create procedure delete_by_id(id int) 
+create procedure delete_by_p_id(in p_id int) 
 begin
 delete from products 
-where products.id = id;
+where products.p_id = p_id;
 end //
 delimiter ;
 
-call delete_by_id(4);
+call delete_by_p_id(4);
 
 call all_information_product();
